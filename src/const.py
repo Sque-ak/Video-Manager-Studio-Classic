a64ccb9a36b15edd1f08c2f49c2adda60fa22c70
Vladimir Novotorzhin <include.dos.h@yandex.ru>
2020-06-01 23:08:30 +0600

    alone canvas finally and first img!

--- src/const.py
+++ src/const.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+import PIL.Image, PIL.ImageTk
 
 #This is global constants declaration;
 NAMEAPP = "Video Manager Studio Classic"
 VERSION = "0.2"
 
-#These variables may change in the future.
+#These variables may change in the future;
 MAXCAMERAS = 16
 SIZE_VIEWCAM = 200
 STATUSAPP = "Good"
 
 #Color list;
 FIRSTCOLOR = "#252525"
-SECONDCOLOR = "#404040"
+SECONDCOLOR = "#303030"
 THREECOLOR = "#5A805A"
+
+#Camera list;
+LISTCAM = {
+        'Camera_0': 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_175k.mov',
+        'Camera_1': 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_175k.mov'}
+
