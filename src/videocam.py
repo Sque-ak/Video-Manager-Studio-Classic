30abf68c7dae7bc4aa239495014078d1c8e484fd
Vladimir Novotorzhin <include.dos.h@yandex.ru>
2020-06-07 11:02:07 +0600

    modify code

--- src/videocam.py
+++ src/videocam.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
 import cv2
 import const
+import tkinter
+import PIL.Image, PIL.ImageTk
 
 """
 Make a videocamera;
 
 @param {String} url : RTSP url;
+@param {Object} canvas: Canvas for drawing the camera;
 @return {Object};
 @example VideoCamera("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_175k.mov");
 """
 class VideoCamera:
-    def __init__(self, url = 0):
-        self.urlCam = cv2.VideoCapture(url)
-        if not self.urlCam.isOpened():
+    def __init__(self, url = 0, canvas = 0):
+        self._canvas = canvas 
+        self._urlCam = cv2.VideoCapture(url)
+        if not self._urlCam.isOpened():
            raise ValueError("URL rtsp is wrong!", url)
 
-        self.width = self.urlCam.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = self.urlCam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.width = self._urlCam.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self._urlCam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
     #Destroy the camera;
     def __del__(self):
-        if self.urlCam.isOpened():
-            self.urlCam.release()
+        if self._urlCam.isOpened():
+            self._urlCam.release()
 
-    #Get frame with camera;
+    #Get frame of the camera;
     #@return {{Boolean}ret, {array}frame};
     def getFrame(self):
-        ret, frame = self.urlCam.read()
+        ret, frame = self._urlCam.read()
         frame = cv2.resize(frame,(const.SIZE_VIEWCAM, const.SIZE_VIEWCAM))
         #Convert to BGR;
         return (cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    #Update frame of the camera;
+    #@param {integer} indX: index number by x;
+    #@param {integer} indY: index number by y;
+    #@param {object} window: main window for update;
+    def updFrame(self, indX, indY, window):
+        self._stream = PIL.ImageTk.PhotoImage(PIL.Image.fromarray(self.getFrame()))
+        self._canvas.create_image((const.SIZE_VIEWCAM+5)*(indX%4), (const.SIZE_VIEWCAM+5)*(indY//4), image = self._stream, anchor = tkinter.NW)
+        window.after(const.DELAY, func=lambda: self.updFrame(indX, indY, window))
