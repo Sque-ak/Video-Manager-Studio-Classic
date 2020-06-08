c8cc3cb02fd76a5ece27f8ab6d807374823897de
Vladimir Novotorzhin <include.dos.h@yandex.ru>
2020-06-08 22:04:46 +0600

    Desing interface and modify status inspector

--- src/app.py
+++ src/app.py
@@ -20,8 +20,8 @@ class VMS:
         #Parameters;
         self._window = window
         self._window.title(title)
-        #self._window.geometry(geometry)
         self._window.geometry("")
+        self._window.iconbitmap('icon.ico')
 
         self._window.minsize(width = const.SCREEN_W, height=const.SCREEN_H)
         self._window.config(bg=const.FIRSTCOLOR)
@@ -29,46 +29,52 @@ class VMS:
 
         self._cameras = []
   
-        self._imgNovid = PIL.ImageTk.PhotoImage(PIL.Image.open(f"src/img/novid_{const.SIZE_VIEWCAM}.jpg"))
+        self._imgNovid = PIL.ImageTk.PhotoImage(PIL.Image.open(f"src/img/novid_{const.SIZE_VIEWCAM_X}x{const.SIZE_VIEWCAM_Y}.jpg"))
 
         self.nodeStatusbar()
 
         self._canvas = tkinter.Canvas(window, 
-            width = (const.SIZE_VIEWCAM)*(const.MAXCAMERAS//4), 
-            height = (const.SIZE_VIEWCAM)*(const.MAXCAMERAS//4), 
+            width = (const.SIZE_VIEWCAM_X)*(const.MAXCAMERAS//4), 
+            height = (const.SIZE_VIEWCAM_Y)*(const.MAXCAMERAS//4), 
             bg=const.FIRSTCOLOR, highlightthickness=0)
-        self._canvas.pack(side = "right", fill = "y", padx = 10)
+        self._canvas.pack(side = "right", fill = "y", padx = 5)
 
         #Node init;
+        self.onInitDevice()
+
         self.nodeInspector()
         self.nodeToolBar()
         self.nodeNovid()
         #end;
-        self.onInitDevice()
+ 
         self._window.mainloop()
 
     "Section Node Begin;"
     def nodeNovid(self):
         # Area without cameras;
         for i in range(const.MAXCAMERAS):
-            self._canvas.create_image((const.SIZE_VIEWCAM+5)*(i%4), (const.SIZE_VIEWCAM+5)*(i//4), image = self._imgNovid, anchor = tkinter.NW)
+            self._canvas.create_image((const.SIZE_VIEWCAM_X+5)*(i%4), (const.SIZE_VIEWCAM_Y+5)*(i//4), image = self._imgNovid, anchor = tkinter.NW)
 
     def nodeInspector(self):
         #Inspector of cameras;
         self._styleInspector = tkinter.ttk.Style()
         self._styleInspector.configure("Inspector.Treeview.Heading", font=('Arial', 10,'bold')) # Modify the font of the headings
+        self._styleInspector.layout("Inspector.Treeview", [('Inspector.Treeview.treearea', {'sticky': 'nswe'})]) # Remove the borders
 
         self._inspector = tkinter.ttk.Treeview(self._window, style="Inspector.Treeview")
-        self._inspector["columns"] = ("Descriptions", "Status")
+        self._inspector["columns"] = ("Status")
+
+        self._inspector.column("#0", width = 120)
+        self._inspector.column("Status", width = 120)
 
         self._inspector.heading("#0", text = "Name", anchor = tkinter.NW)
-        self._inspector.heading("Descriptions", text = "Descriptions", anchor = tkinter.NW)
         self._inspector.heading("Status", text = "Status", anchor = tkinter.NW)
 
         #Level - 1
-        self._namepack = self._inspector.insert("", 1, text = "Pack", values = ("Test pack", "Connected"))
-        self._inspector.insert(self._namepack, "end", text = "#1 Camera", values = ("Test camera", "Connected"))
-        self._inspector.insert(self._namepack, "end", text = "#2 Camera", values = ("Test camera", "Connected"))
+        self._namepack = self._inspector.insert("", 1, text = "Pack", values = (""))
+        for i in range(len(const.LISTCAM)):
+            self._inspector.insert(self._namepack, "end", text = f"#{i+1} Camera", values = (self._cameras[i].getStatus()))
+
         self._inspector.pack(side = "left", fill = "y")
 
         
